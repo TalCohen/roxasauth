95c4474f296145bc4ca15ac72d0abe9435adccc9
Tal Cohen <tcohen123@gmail.com>
2016-12-14 19:02:21 -0500

    Updated api_key to be in the constructor

--- roxasauth.py
+++ roxasauth.py
@@ -1,22 +1,23 @@
 import requests
 
 class roxasauth():
-    def __init__(self):
+    def __init__(self, api_key):
+        self.api_key = api_key
+
         self.roxas_url = 'http://gravity.csh.rit.edu:3333/'
         self.ibutton_url = 'ibutton/auth'
         self.nfc_auth_url = 'nfc/auth'
         self.nfc_verify_url = 'nfc/verify'
 
     
-    def ibutton(self, api_key, ibutton, attrs=None):
+    def ibutton(self, ibutton, attrs=None):
         """ 
         Authenticate with a user's ibutton.
 
-        @param: api_key - The api key for the device
         @param: ibutton - The user's ibutton/serial number
         @param: attrs - The attributes to return. The entryUUID and uid of the user is always returned.
 
-        @return: If the request was successful, a json object with the fields 'can_access', 'message', 'returned_attributes'.
+        @return: If the request was successful, a json object with the fields 'can_access', 'message', 'returned_attrs'.
                  If the request was unsuccessful, None.
         """
 
@@ -25,7 +26,7 @@ class roxasauth():
 
         # Create the payload
         data = {
-            'api_key': api_key,
+            'api_key': self.api_key,
             'ibutton': ibutton,
             'attrs': attrs,
         }
@@ -39,11 +40,11 @@ class roxasauth():
         else: 
             return None
 
-    def nfc(self, secret_key, api_key, serial_number, rolling_key, attrs=None):
+    def nfc(self, secret_key, serial_number, rolling_key, attrs=None):
         url = self.roxas_url + self.nfc_auth_url
         data = {
             'secret_key': secret_key,
-            'api_key': api_key,
+            'api_key': self.api_key,
             'serial_number': serial_number,
             'rolling_key': rolling_key,
             'attrs': attrs,
